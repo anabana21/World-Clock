dcf50cfd5461db46e4a500d0cefc73260494b9aa
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-03-22 18:07:58 +0100

    added js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  //Zurich
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDateElement = zurichElement.querySelector(".date");
+  let zurichTimeElement = zurichElement.querySelector(".time");
+  let zurichTime = moment().tz("Europe/Zurich");
 
+  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+  zurichTimeElement.innerHTML = zurichTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
